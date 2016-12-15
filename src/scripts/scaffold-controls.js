05d90e1ad4eb6e34add5f5a2f4ff7c6db1005ba4
Phil Hawksworth <phawksworth@gmail.com>
2016-12-15 15:55:31 +0000

    per section classes

--- src/scripts/scaffold-controls.js
+++ src/scripts/scaffold-controls.js
@@ -3,19 +3,19 @@ var toggle = document.querySelectorAll('.page-purpose__toggle');
 document.addEventListener('click', function(event){
 
     if(event.target.classList.contains('page-purpose__toggle')) {
+
         event.preventDefault();
+        event.target.blur();
+
+        // toggle the infoe drawer
         var content = document.querySelector('.page-purpose .container');
         content.classList.toggle('hide');
-        event.target.blur();
-    }
-    else if(event.target.classList.contains('page-sections__toggle')) {
-        event.preventDefault();
+
+        // toggle the sections info
         var sections = document.querySelectorAll('section');
         for(var section in sections) {
             sections[section].classList.toggle('quiet');
         }
-        event.target.blur();
     }
     
-
 }, false);
